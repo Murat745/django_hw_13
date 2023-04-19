5d2f2faa7f2616706761b102141fe0d9fc07fd07
I.Muratov <ivanmuratov.2016@gmail.com>
2023-04-19 22:38:48 +0300

    celery_email app created

---
+++ celery_email/views.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .forms import ReminderForm
+from .tasks import send_email
+
+
+def sender(request):
+    if request.method == "POST":
+        form = ReminderForm(request.POST)
+
+        if form.is_valid():
+            to_email = form.cleaned_data['to_email']
+            text_reminder = form.cleaned_data['text_reminder']
+            date_to_remind = form.cleaned_data['date_to_remind']
+            current_time = timezone.now()
+
+            if date_to_remind - current_time < timedelta(days=2):
+                send_email.apply_async(args=(to_email, text_reminder), eta=date_to_remind)
+
+                return redirect('celery_email:success')
+
+    else:
+        form = ReminderForm()
+
+    return render(request, 'celery_email/reminder.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'celery_email/success.html')
