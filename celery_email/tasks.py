e880342853f569e8c8b6417cf4a6bb3c0336f68a
I.Muratov <ivanmuratov.2016@gmail.com>
2023-04-24 21:32:53 +0300

    celery_email app modified

--- celery_email/tasks.py
+++ celery_email/tasks.py
@@ -5,4 +5,10 @@ from django.core.mail import send_mail as django_send_mail
 
 @shared_task
 def send_email(text_reminder, to_email):
-    django_send_mail(text_reminder, to_email)
+    django_send_mail(
+        'Subject',
+        text_reminder,
+        'example@outlook.com',
+        ['list@example.com'],
+        to_email,
+    )
