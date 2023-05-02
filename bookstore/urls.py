70a68ef792a42472829e9cee3f516277787b1a3d
I.Muratov <ivanmuratov.2016@gmail.com>
2023-05-02 23:03:02 +0300

    bookstore app's class based views created bookstore app's forms created urls modified new html templates created for create, update, delete a book

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -5,8 +5,11 @@ from . import views
 app_name = 'bookstore'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.book_list, name='book_list'),
-    path('books/<int:pk>', views.book, name='book'),
+    path('books/', views.BookListView.as_view(), name='book_list'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book'),
+    path('books/create', views.BookCreateView.as_view(), name='create_book'),
+    path('books/<int:pk>/update', views.BookUpdateView.as_view(), name='update_book'),
+    path('books/<int:pk>/delete', views.BookDeleteView.as_view(), name='delete_book'),
     path('authors/', views.author_list, name='author_list'),
     path('authors/<int:pk>', views.author, name='author'),
     path('publishers/', views.publisher_list, name='publisher_list'),
